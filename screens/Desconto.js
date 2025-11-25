29b4e8c0c071cbec1fa55009b324c5979a7a3057
aureliovianadev <aureliovianadev@gmail.com>
2025-11-24 22:32:24 -0300

    Criando tela de desconto

---
+++ screens/Desconto.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+
+export default function Desconto() {
+
+    const [preco, setPreco] = useState('')
+    const [porcentagem, setPorcentagem] = useState('')
+    const [preco_final, setPreco_final] = useState('')
+
+    function calculardesconto() {
+        setPreco_final(parseFloat(preco)-(parseFloat(preco) * parseInt(porcentagem/100)))
+    }
+    
+    
+    return (
+        <View>
+            <Text>Desconto</Text>
+
+            <TextInput
+                placeholder="Digite o valor do produto"
+                value={preco}
+                onChangeText={setPreco}
+            />
+
+            <TextInput
+                placeholder="Digite quantos porcentos o produto tem de desconto"
+                value={porcentagem}
+                onChangeText={setPorcentagem}
+            />
+
+            <View style={estilos.botao}>
+                <Button
+                    title="Calcular"
+                    onPress={() => calculardesconto()}
+                />
+            </View>
+            
+            <Text>preco_final: {preco_final}</Text>
+
+        </View>
+    )
+}
+
+const estilos = StyleSheet.create({
+    botao: {
+        marginVertical: 10
+    }
+})
